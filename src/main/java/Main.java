378f674461f920f5b6bf1bf5d5adc6f278ec1450
Ethan Tumbocon <ethan.tumbocon1@gmail.com>
2021-04-07 23:31:06 -0500

    calling bot in discord server

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,8 @@ import
 public class Main {
     public static void main(String[] args){
 JDAbuilder builder = new JDABuilder(AccountType.BOT);
-
+String token =
+        builder.setToken(token);
+builder.buildAsync();
     }
 }
